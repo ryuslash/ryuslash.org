ad30e70b9275eb6fa68677645816180e3b8a3cab
Tom Willemsen <tom@ryuslash.org>
2012-03-12 01:53:15 +0100

    Remove unnecessary apps

--- settings.py
+++ settings.py
@@ -111,15 +111,7 @@ TEMPLATE_DIRS = (
     '%s/templates' % DEPLOY_PATH,
 )
 
-INSTALLED_APPS = ('django.contrib.auth',
-                  'django.contrib.contenttypes',
-                  'django.contrib.sessions',
-                  'django.contrib.sites',
-                  'django.contrib.messages',
-                  'django.contrib.staticfiles',
-                  'django.contrib.admin',
-                  'django.contrib.admindocs',
-                  'aggregator',
+INSTALLED_APPS = ('aggregator',
                   'south')
 
 # A sample logging configuration. The only tangible logging
