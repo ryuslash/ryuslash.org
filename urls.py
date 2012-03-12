2dab9254e1da14db38c0862c44bace2ad4993bda
Tom Willemsen <tom@ryuslash.org>
2012-03-12 01:45:07 +0100

    Add seperate page per post

--- urls.py
+++ urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.views.generic import DetailView
 from django.contrib import admin
+
+from aggregator.models import Post
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^((?P<page>\d+)/)?$', 'aggregator.views.posts'),
+    url(r'^post/((?P<pk>\d+)/)?$', DetailView.as_view(model=Post)),
     url(r'^admin/', include(admin.site.urls)))
 
 urlpatterns += staticfiles_urlpatterns()
