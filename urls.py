f7b77e38325ca1fc3dbb13373b81123f4ead97f9
Tom Willemsen <tom@ryuslash.org>
2012-03-13 01:23:42 +0100

    Add RSS feed for comments

--- urls.py
+++ urls.py
@@ -3,12 +3,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import DetailView
 
 from aggregator.models import Post
-from aggregator.feeds import LatestPostsFeed
+from aggregator.feeds import LatestPostsFeed, LatestCommentsFeed
 
 urlpatterns = patterns('',
     url(r'^((?P<page>\d+)/)?$', 'aggregator.views.posts'),
     url(r'^post/((?P<pk>\d+)/)?$', DetailView.as_view(model=Post)),
     url(r'^feed/posts/$', LatestPostsFeed()),
+    url(r'^feed/comments/$', LatestCommentsFeed()),
     url(r'^comments/', include('django.contrib.comments.urls')))
 
 urlpatterns += staticfiles_urlpatterns()
