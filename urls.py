74c070279fb1c240779f9562ab6171ba32e3d744
Tom Willemsen <tom@ryuslash.org>
2012-03-27 22:44:39 +0200

    Add categories to main page

--- urls.py
+++ urls.py
@@ -6,7 +6,8 @@ from aggregator.models import Post
 from aggregator.feeds import LatestPostsFeed, LatestCommentsFeed
 
 urlpatterns = patterns('',
-    url(r'^((?P<page>\d+)/)?$', 'aggregator.views.posts'),
+    url(r'^((?P<cat>[a-z_-]+)/)?((?P<page>\d+)/)?$',
+        'aggregator.views.posts'),
     url(r'^post/((?P<pk>\d+)/)?$', DetailView.as_view(model=Post)),
     url(r'^feed/posts/$', LatestPostsFeed()),
     url(r'^feed/comments/$', LatestCommentsFeed()),
