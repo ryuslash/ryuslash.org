44cdd17a88595e16a9fd43f55e9dac38a0047e6b
Tom Willemsen <tom@ryuslash.org>
2012-05-21 22:26:37 +0200

    Move templates
    Moved templates into the aggregator app's folder.

--- settings.py
+++ settings.py
@@ -108,7 +108,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '%s/templates' % DEPLOY_PATH,
 )
 
 INSTALLED_APPS = ('django.contrib.contenttypes',
