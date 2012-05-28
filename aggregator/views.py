d0426e377f52196971b9423be42c3f4980664b64
Tom Willemsen <tom@ryuslash.org>
2012-05-28 12:31:30 +0200

    Remove unused import

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,7 +1,6 @@
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.http import Http404
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
 
 from .models import Post
 
