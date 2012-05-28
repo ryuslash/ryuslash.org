53ab83c5c20d746e31e003a3d84e3a21c0e0200c
Tom Willemsen <tom@ryuslash.org>
2012-05-28 12:31:14 +0200

    Make posts link to origin
    Removes the "origin" link and changes the link from the title to the
    origin of the post.

--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import DetailView
 
-from aggregator.models import Post
 from aggregator.feeds import LatestPostsFeed
 
 urlpatterns = patterns('',
-    url(r'^post/(?P<pk>\d+)/$', DetailView.as_view(model=Post)),
     url(r'^((?P<cat>[a-z_-]+)/)?((?P<page>\d+)/)?$',
         'aggregator.views.posts'),
     url(r'^feed/posts/$', LatestPostsFeed()))
