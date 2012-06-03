e4f6b3bd35a8f79df47f264bf9fc7f63111a8af4
Tom Willemsen <tom@ryuslash.org>
2012-06-03 22:47:44 +0200

    Add truncate and update nameless
    I don't want to see `by-ryuslash' any more than I want to see
    `ryuslash-', so I changed the regex.
    
    Also added a filter to truncate variables, since places like identica
    just send the message as the title it can get quite long.

--- aggregator/templatetags/posts_extras.py
+++ aggregator/templatetags/posts_extras.py
@@ -7,6 +7,14 @@ register = template.Library()
 
 @stringfilter
 def nameless(value):
-    return re.sub(r'^(ryuslash|tom)[- ]?', '', value)
+    return re.sub(r'(^|by[- ])(ryuslash|tom)[- ]?', '', value)
+
+@stringfilter
+def truncate(value, length):
+    if len(value) > length:
+        value = value[:length-3] + '...'
+
+    return value
 
 register.filter('nameless', nameless)
+register.filter('truncate', truncate)
